fa53ecc8ae8e165f39d277fee891e1e13b4aefdb
caizhanfei <caizhanfei@126.com>
2023-03-14 20:09:43 -0400

    1 file added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import './style.css';
-import Column from './Column';
+import { Column } from './StyledComponents';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <Column></Column>
+      <Column>あいうえお</Column>
     </div>
   );
 }
--- Column.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import './font.css';
-
-const Column = styled.div`
-  writing-mode: vertical-rl;
-  font-size: 100px;
-`;
-
-export default Column;
